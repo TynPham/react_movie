479943c90f33abfa906deac904240429c9120b59
pham tuyen <phamtuyendz1104@gmai.com>
2022-09-10 21:27:03 +0700

    ((^_^))

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
   baseUrl: "https://api.themoviedb.org/3/",
-  apiKey: "", // take on themoviedb.org
+  apiKey: "3b92846b32d91a3d43f1b663a727fcc2",
   originalImage: (imgpath) => `https://image.tmdb.org/t/p/original/${imgpath}`,
   w500img: (imgpath) => `https://image.tmdb.org/t/p/w500/${imgpath}`,
 };
